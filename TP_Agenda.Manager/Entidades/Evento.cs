7761a564eac015bc7f6d61d746f186a79afe5223
Nicobuggenthin <buggenthin.15@gmail.com>
2024-11-23 18:42:49 -0300

    Primer commit: Subida inicial del proyecto TP-Agenda

---
+++ TP_Agenda.Manager/Entidades/Evento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Agenda.Manager.Entidades
+{
+    public class Evento
+    {
+        public int IdEventos { get; set; }
+        public string Titulo { get; set; }
+        public DateTime Fecha { get; set; }
+        public string? Descripcion { get; set; } //Puede ser nulo
+        public string? Ubicacion { get; set; } //Puede ser nulo
+        public string UsuarioId { get; set; } //Relacion con la tabla ASPNetUsers
+        public string Estado { get; set; } = "Pendiente"; //Valor por defecto
+
+    }
+}
